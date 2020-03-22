8763e10e6306e96f8b37b3a1d2afd5728819dd78
Andrew <andrewkarp00@gmail.com>
2020-03-23 00:03:31 +0200

    Added delete button for messages

--- src/components/MessageList.vue
+++ src/components/MessageList.vue
@@ -3,7 +3,6 @@
     class="mb-8"
     subheader
     two-line
-    disabled
     elevation="4"
     min-width="280px"
     style="width: 60%"
@@ -14,16 +13,25 @@
         <v-list-item-title>{{ message.name }}</v-list-item-title>
         <v-list-item-subtitle>{{ message.text }}</v-list-item-subtitle>
       </v-list-item-content>
+      <v-list-item-action>
+        <v-icon dense @click="deleteMessage(message)">mdi-delete</v-icon>
+      </v-list-item-action>
     </v-list-item>
   </v-list>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import { messagesRef } from "../configs/firebase";
 
 export default {
   computed: {
     ...mapGetters(["messages"])
+  },
+  methods: {
+    deleteMessage(message) {
+      messagesRef.child(message.id).remove();
+    }
   }
 };
 </script>
