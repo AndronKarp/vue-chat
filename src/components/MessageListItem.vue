b9497ae32af0676bf169bf67197c92caa057b905
Andrew <andrewkarp00@gmail.com>
2020-03-24 19:27:05 +0200

    Added v-hover

--- src/components/MessageListItem.vue
+++ src/components/MessageListItem.vue
@@ -1,21 +1,23 @@
 <template>
-  <v-list-item>
-    <v-row class="px-3" v-if="!isEditing">
-      <v-list-item-content>
-        <v-list-item-title>{{ message.text }}</v-list-item-title>
-        <v-list-item-subtitle>{{ message.name }}</v-list-item-subtitle>
-      </v-list-item-content>
-      <v-list-item-action class="flex-row">
-        <v-icon class="mr-2" dense @click="editMessage">mdi-pencil</v-icon>
-        <v-icon dense @click="deleteMessage(message)">mdi-delete</v-icon>
-      </v-list-item-action>
-    </v-row>
-    <EditMessageForm
-      v-else
-      :message="message"
-      @onCancelEditing="cancelEditing"
-    />
-  </v-list-item>
+  <v-hover v-slot:default="{ hover }">
+    <v-list-item>
+      <v-row class="px-3" v-if="!isEditing">
+        <v-list-item-content>
+          <v-list-item-title>{{ message.text }}</v-list-item-title>
+          <v-list-item-subtitle>{{ message.name }}</v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action :hidden="!hover" class="flex-row align-center">
+          <v-icon class="mr-2" dense @click="editMessage">mdi-pencil</v-icon>
+          <v-icon dense @click="deleteMessage(message)">mdi-delete</v-icon>
+        </v-list-item-action>
+      </v-row>
+      <EditMessageForm
+        v-else
+        :message="message"
+        @onCancelEditing="cancelEditing"
+      />
+    </v-list-item>
+  </v-hover>
 </template>
 
 <script>
