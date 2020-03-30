1248d0bce77ba435b243f6b96e4e9a2fba380696
Andrew <andrewkarp00@gmail.com>
2020-03-30 19:33:07 +0300

    Deleted name input in SendMessageForm component

--- src/components/SendMessageForm.vue
+++ src/components/SendMessageForm.vue
@@ -5,14 +5,8 @@
   >
     <v-text-field
       v-model="$v.form.text.$model"
+      type="text"
       placeholder="Write your message..."
-      outlined
-      dense
-      style="width: 100%"
-    ></v-text-field>
-    <v-text-field
-      v-model="$v.form.name.$model"
-      placeholder="Your name..."
       outlined
       dense
       style="width: 100%"
@@ -31,25 +25,31 @@
 import { validationMixin } from "vuelidate";
 import { required } from "vuelidate/lib/validators";
 import { messagesRef } from "../configs/firebase";
+import { mapGetters } from "vuex";
 
 export default {
   data() {
     return {
       form: {
-        name: null,
         text: null
       }
     };
   },
   validations: {
     form: {
-      name: { required },
       text: { required }
     }
   },
+  computed: {
+    ...mapGetters(["currentUser"])
+  },
   methods: {
     sendMessage() {
-      messagesRef.push({ name: this.form.name, text: this.form.text });
+      this.$v.form.$touch();
+      messagesRef.push({
+        name: this.currentUser.displayName,
+        text: this.form.text
+      });
       this.form.text = null;
     }
   },
