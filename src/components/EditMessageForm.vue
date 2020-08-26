09918f1b73e731652a3b9f2edffa0b255369720c
Andrew <andrewkarp00@gmail.com>
2020-08-26 20:26:07 +0300

    refactored EditMessageForm component

--- src/components/EditMessageForm.vue
+++ src/components/EditMessageForm.vue
@@ -1,35 +1,20 @@
 <template>
-  <v-form
-    @submit.prevent="editMessage(message)"
-    class="d-flex flex-column align-center py-2"
-    style="width: 100%"
-  >
-    <v-text-field
-      outlined
-      dense
-      clearable
-      v-model="$v.editingMessage.text.$model"
-      style="width: 100%"
-    ></v-text-field>
-    <div class="buttons d-flex">
-      <v-btn
-        type="submit"
-        :dark="!$v.editingMessage.text.$invalid"
-        :disabled="$v.editingMessage.text.$invalid"
-        color="amber darken-4"
-        >Save</v-btn
-      >
-      <v-btn class="ml-4" @click="cancelEditing" dark color="amber darken-4"
-        >Cancel</v-btn
-      >
-    </div>
+  <v-form @submit.prevent="editMessage(message)" class="flex-grow-1">
+    <v-text-field outlined dense v-model="text">
+      <template #append-outer>
+        <v-btn icon small :disabled="isMessageEmpty" type="submit">
+          <v-icon>mdi-check</v-icon>
+        </v-btn>
+        <v-btn icon small @click="cancelEditing">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </template>
+    </v-text-field>
   </v-form>
 </template>
 
 <script>
 import { messagesRef } from "../configs/firebase";
-import { validationMixin } from "vuelidate";
-import { required } from "vuelidate/lib/validators";
 
 export default {
   props: {
@@ -40,12 +25,12 @@ export default {
   },
   data() {
     return {
-      editingMessage: { ...this.message }
+      text: this.message.text
     };
   },
-  validations: {
-    editingMessage: {
-      text: { required }
+  computed: {
+    isMessageEmpty() {
+      return this.text.length === 0;
     }
   },
   methods: {
@@ -53,13 +38,10 @@ export default {
       this.$emit("onCancelEditing");
     },
     async editMessage(message) {
-      await messagesRef
-        .child(message.id)
-        .update({ text: this.editingMessage.text });
+      await messagesRef.child(message.id).update({ text: this.text });
       this.cancelEditing();
     }
-  },
-  mixins: [validationMixin]
+  }
 };
 </script>
 
