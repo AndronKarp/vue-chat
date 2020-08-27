1391bc524835a996c87e860fc69e175ac3fbb50d
Andrew Karpenko <andrewkarp00@gmail.com>
2020-08-27 19:41:27 +0300

    moved sign out button to nav

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,16 +2,11 @@
   <v-app-bar app color="amber darken-4" dark>
     <v-app-bar-nav-icon @click="toggleNav" />
     <v-toolbar-title>Vue Chat</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-btn v-if="currentUser" @click="signOut" icon>
-      <v-icon>mdi-logout</v-icon>
-    </v-btn>
   </v-app-bar>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-import { auth } from "../configs/firebase";
 
 export default {
   computed: {
@@ -20,10 +15,6 @@ export default {
   methods: {
     toggleNav() {
       this.$emit("navButtonClick");
-    },
-    async signOut() {
-      await auth.signOut();
-      this.$router.push("/auth");
     }
   }
 };
--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,38 +1,19 @@
 <template>
   <v-navigation-drawer :value="value" @input="onInput" app>
     <v-list dense>
-      <v-list-item link to="/">
+      <v-list-item @click="signOut">
         <v-list-item-action>
-          <v-icon>mdi-home</v-icon>
+          <v-icon>mdi-logout</v-icon>
         </v-list-item-action>
-        <v-list-item-content>
-          <v-list-item-title>Home</v-list-item-title>
-        </v-list-item-content>
+        <v-list-item-content>Sign out</v-list-item-content>
       </v-list-item>
-      <template v-if="!currentUser">
-        <v-list-item link to="/reg">
-          <v-list-item-action>
-            <v-icon>mdi-account-plus</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Sign Up</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link to="/auth">
-          <v-list-item-action>
-            <v-icon>mdi-login</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Sign In</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </template>
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import { auth } from "../configs/firebase";
 
 export default {
   props: ["value"],
@@ -42,6 +23,10 @@ export default {
   methods: {
     onInput(value) {
       this.$emit("input", value);
+    },
+    async signOut() {
+      await auth.signOut();
+      this.$router.push("/auth");
     }
   }
 };
