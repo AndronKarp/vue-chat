2023cc609bda6165ce52ef808249d92219f35e69
Andrew <andrewkarp00@gmail.com>
2020-09-04 17:09:06 +0300

    created ChatList component

---
+++ src/components/ChatList.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card tile>
+    <v-toolbar color="amber darken-4" dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Vue Chat</v-toolbar-title>
+    </v-toolbar>
+    <v-list two-line>
+      <template v-for="(item, index) in items">
+        <v-list-item :key="item.title">
+          <v-list-item-avatar color="amber darken-4">
+            <v-icon color="white">mdi-account</v-icon>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-subtitle>
+              {{ item.subtitle }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider v-if="index + 1 < items.length" :key="index"></v-divider>
+      </template>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: "John Doe", subtitle: "Lorem Ipsum Dolor Sit Amet" },
+        { title: "John Doe", subtitle: "Lorem Ipsum Dolor Sit Amet" },
+        { title: "John Doe", subtitle: "Lorem Ipsum Dolor Sit Amet" },
+        { title: "John Doe", subtitle: "Lorem Ipsum Dolor Sit Amet" }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
