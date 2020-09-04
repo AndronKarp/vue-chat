e9038c896bb919d1d5b27e34c1af494fe22ae90e
Andrew <andrewkarp00@gmail.com>
2020-09-04 19:50:14 +0300

    set position absolute of ChatList toolbar

--- src/components/ChatList.vue
+++ src/components/ChatList.vue
@@ -1,10 +1,16 @@
 <template>
-  <v-card tile>
-    <v-toolbar color="amber darken-4" dark>
+  <v-card class="fill-height" tile style="position: relative">
+    <v-toolbar
+      color="amber darken-4"
+      flat
+      dark
+      absolute
+      height="56px"
+      width="100%"
+    >
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-toolbar-title>Vue Chat</v-toolbar-title>
     </v-toolbar>
-    <v-list two-line>
+    <v-list class="pt-14" two-line max-height="100vh" style="overflow-y: auto">
       <template v-for="(item, index) in items">
         <v-list-item :key="item.title">
           <v-list-item-avatar color="amber darken-4">
@@ -17,7 +23,7 @@
             </v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
-        <v-divider v-if="index + 1 < items.length" :key="index"></v-divider>
+        <v-divider :key="index"></v-divider>
       </template>
     </v-list>
   </v-card>
